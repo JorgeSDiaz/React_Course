43e2f6418ed4e657fe70e662d1976a241533917f
Jorge <saenzdiazj0@gmail.com>
2023-06-01 00:55:53 -0500

    Event handlers

--- src/index.js
+++ src/index.js
@@ -1,21 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import UserCard, { Generic } from "./Generic";
-import { Product } from "./Product";
-import { Task } from "./Task";
-import Hello from "./Hello";
+
+import { Button } from "./Button";
+// import UserCard, { Generic } from "./Generic";
+// import { Product } from "./Product";
+// import { Task } from "./Task";
+// import Hello from "./Hello";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+function handleClick() {
+  console.log("click");
+}
+
 root.render(
   <>
-    <Hello />
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        alert("Submitted: " + e.target[0].value);
+      }}
+    >
+      <input
+        type="text"
+        onClick={() => {
+          alert("Selected");
+        }}
+        onChange={(event) => {
+          console.log(event.target.value);
+        }}
+      />
+      <button type="submit">Submit</button>
+    </form>
+    <Button onClick={handleClick} />
+    {/* <Hello />
     <Task tittle="Task completed" finished={true}/>
     <Task tittle="Task not completed"/>
     <br/>
     <Generic />
     <UserCard name="Jorge" age={21} isMale={true} />
     <Product producName="Nutella" price={500} description="Esta rica" />
-    <Product price={0} />
+    <Product price={0} /> */}
   </>
 );
