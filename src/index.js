bdae413c8868a435dbb98cf24852714c41008268
Jorge <saenzdiazj0@gmail.com>
2023-06-02 22:10:48 -0500

    Arrays

--- src/index.js
+++ src/index.js
@@ -1,47 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { Post } from "./Post";
-// import { Button } from "./Button";
-// import UserCard, { Generic } from "./Generic";
-// import { Product } from "./Product";
-// import { Task } from "./Task";
-// import Hello from "./Hello";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// function handleClick() {
-//   console.log("click");
-// }
+const users = [
+  {
+    id: 1,
+    name: "John",
+    image: "https://robohash.org/user1",
+  },
+  {
+    id: 2,
+    name: "Joe",
+    image: "https://robohash.org/user2",
+  },
+  {
+    id: 3,
+    name: "Joseph",
+    image: "https://robohash.org/user3",
+  },
+];
 
 root.render(
   <>
-    <Post />
-    {/* <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        alert("Submitted: " + e.target[0].value);
-      }}
-    >
-      <input
-        type="text"
-        onClick={() => {
-          alert("Selected");
-        }}
-        onChange={(event) => {
-          console.log(event.target.value);
-        }}
-      />
-      <button type="submit">Submit</button>
-    </form>
-    <Button onClick={handleClick} /> */}
-    {/* <Hello />
-    <Task tittle="Task completed" finished={true}/>
-    <Task tittle="Task not completed"/>
-    <br/>
-    <Generic />
-    <UserCard name="Jorge" age={21} isMale={true} />
-    <Product producName="Nutella" price={500} description="Esta rica" />
-    <Product price={0} /> */}
+    {users.map((user) => {
+      return (
+        <div key={user.id}>
+          <h1>{user.name}</h1>
+          <img src={user.image} alt={user.name} />
+        </div>
+      );
+    })}
   </>
 );
